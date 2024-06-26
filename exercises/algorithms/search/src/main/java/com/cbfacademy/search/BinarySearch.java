f70d3bafc2f7b36212df23df2d06b27cc0b6c5bd
Kelly Nya <knya819@gmail.com>
2024-06-26 16:19:43 +0100

    Done new exercises

---
+++ exercises/algorithms/search/src/main/java/com/cbfacademy/search/BinarySearch.java
@@ -0,0 +1,22 @@
+public class BinarySearch {
+    public static int binarySearch(int[] sortedArray, int target){
+        
+        int left = 0;
+        int right = sortedArray.length -1;
+        
+        while (left <= right){
+            int mid = left + (right-left)/2;
+
+            if (sortedArray[mid] == target){
+                return mid;
+            }
+            if (sortedArray[mid] < target){
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1 ;
+    }
+}
---
+++ exercises/algorithms/sort/src/main/java/com/cbfacademy/sort/QuickSort.java
@@ -0,0 +1,34 @@
+package com.cbfacademy.sort;
+
+public class QuickSort {
+    public static void sort(int[] arr, int startIndex, int endIndex){
+        if (startIndex < endIndex){
+           int pivotIndex = partition(arr, startIndex, endIndex);
+            sort(arr, startIndex, pivotIndex - 1);
+            sort(arr, pivotIndex + 1, endIndex);
+        }
+    }
+
+    private static int partition(int arr[], int startIndex, int endIndex){
+        int pivot = arr[endIndex];
+        int boundaryIndex = startIndex - 1;
+        for (int i = startIndex; i < endIndex; i ++){
+            if (arr[i] <= pivot){
+                 boundaryIndex = boundaryIndex + 1;
+                swap(arr, boundaryIndex, i);
+            }
+        }
+        swap(arr, boundaryIndex + 1, endIndex);
+        return boundaryIndex + 1;
+    }
+
+    // We are given the input array, the position of the left-hand element and the position of the right-hand element
+    public static void swap(int[] arr, int leftIndex, int rightIndex){
+    // Store the value of the left-hand element
+       int temp = arr[leftIndex];
+    // Assign the value of the right-hand element to the left-hand element
+        arr[leftIndex] = arr[rightIndex];
+    // Assign the original value of the left-hand element to the right-hand element
+        arr[rightIndex] = temp;
+    }
+}
