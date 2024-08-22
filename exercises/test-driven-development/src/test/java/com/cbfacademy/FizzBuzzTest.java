9af80bda289d76769c5005a576866c5096533e91
Knya819 <knya819@gmail.com>
2024-08-22 13:25:04 +0100

    Modified FizzBuzzTest.java

--- exercises/test-driven-development/src/test/java/com/cbfacademy/FizzBuzzTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/FizzBuzzTest.java
@@ -9,19 +9,11 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class FizzBuzzTest {
-
-    @Test
-    @DisplayName("creates the app")
-    public void createsAnApp() {
-        final App app = new App();
-        assertThat(app, is(notNullValue()));
-    }
 
     // Single number tests
     static Stream<Arguments> singleNumberTests() {
