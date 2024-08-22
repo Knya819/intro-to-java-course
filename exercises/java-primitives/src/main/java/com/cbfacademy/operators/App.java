d0fdfd426ff69224f65674b4466701bd82e24e1b
Knya819 <knya819@gmail.com>
2024-08-22 11:13:58 +0100

    Review primitive type: Operators

--- exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
@@ -1,37 +1,37 @@
 package com.cbfacademy.operators;
 
+import java.lang.*;;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 
     public static double add(double operand1, double operand2) {
-        //throw new RuntimeException("Not implemented");
-        return operand1 + operand2 ;
+        double somma = operand1 + operand2;
+        return somma ;
     }
 
     public static double subtract(double operand1, double operand2) {
-       // throw new RuntimeException("Not implemented");
-       return operand1 - operand2 ;
+       double subtract = operand1 - operand2;
+       return subtract ;
     }
 
     public static double multiply(double operand1, double operand2) {
-       // throw new RuntimeException("Not implemented");
-       return operand1 * operand2 ;
+       double multiply = operand1 * operand2;
+       return multiply ;
     }
 
     public static Boolean areEqual(double operand1, double operand2) {
-       // throw new RuntimeException("Not implemented");
-       return operand1 == operand2;
+        boolean equality = operand1 == operand2;
+        return equality;
     }
 
     public static Boolean isLessThan(double operand1, double operand2) {
-       // throw new RuntimeException("Not implemented");
-       return operand1 < operand2 ;
+       return operand1 < operand2;
     }
 
     public static Boolean isMoreThan(double operand1, double operand2) {
-        //throw new RuntimeException("Not implemented");
-        return operand1 > operand2 ;
+        return operand1 > operand2;
     }
 }
