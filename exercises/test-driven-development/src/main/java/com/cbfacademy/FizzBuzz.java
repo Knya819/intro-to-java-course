7d5f66a6159e8b5f50479aa07f72479a277f1e86
Knya819 <knya819@gmail.com>
2024-08-22 13:11:01 +0100

    Added List option

--- exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -1,17 +1,25 @@
 package com.cbfacademy;
 
-public class FizzBuzz {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static Object get(Integer i) {
-     // return (i%3 == 0) ? "Fizz" : Integer.toString(i);
-     if (i%3 == 0) {
-        return "Fizz";
-     } else if (i%5 == 0) {
-     return "Buzz";
-     }
-     else {
-        return Integer.toString(i);
-     }
+public class FizzBuzz {
+   // For a single number
+   public static String get(Integer i) {
+      if (i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
+      else if (i % 3 == 0) return "Fizz";
+      else if (i % 5 == 0) return "Buzz";
+      else return i.toString();
   }
 
+   // For a list of numbers
+   public List<String> fizzBuzz(List<Integer> numbers) {
+       List<String> result = new ArrayList<>();
+       for (Integer i : numbers) {
+           result.add(get(i));
+       }
+       return result;
+   }
+
+   
 }
