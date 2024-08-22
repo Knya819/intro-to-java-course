ac52fbde45fcaf6e2b72417671a65c0f026f0b6d
Knya819 <knya819@gmail.com>
2024-08-22 13:27:54 +0100

    Modified QuickSortTest.java

--- exercises/algorithms/sort/src/main/java/com/cbfacademy/sort/QuickSort.java
+++ exercises/algorithms/sort/src/main/java/com/cbfacademy/sort/QuickSort.java
@@ -4,7 +4,7 @@ public class QuickSort implements ArraySorter {
     //@Override
     public  void sort(int[] arr, int startIndex, int endIndex){
         if (startIndex < endIndex){
-           int pivotIndex = Partition.partition(arr, startIndex, endIndex);
+           int pivotIndex = SortMethods.partition(arr, startIndex, endIndex);
             sort(arr, startIndex, pivotIndex - 1);
             sort(arr, pivotIndex + 1, endIndex);
         }
