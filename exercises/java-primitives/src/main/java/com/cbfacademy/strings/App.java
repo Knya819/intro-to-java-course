b8cc2fbcac40bf8e11799960b1799bb21861f291
Knya819 <knya819@gmail.com>
2024-08-22 10:41:47 +0100

    Review primitive type: String

--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -5,22 +5,21 @@ public class App {
     }
 
     public static String concatenate(String word1, String word2, String word3) {
-        // TODO: Write code that concatenates the input parameters and returns the result
-         return word1 + word2 + word3;
-        //throw new RuntimeException("Not implemented");
-        //System.out.println(concat);
+        String first2 = word1.concat(word2);
+            return first2.concat(word3) ;
     }
 
     public static Boolean areEqual(String word1, String word2) {
-        // TODO: Write code to determine whether the input parameters are equal strings
-        return word1.equals(word2) ;
-        //throw new RuntimeException("Not implemented");
+        Boolean equality = word1.equals(word2);
+            return equality ;
     }
 
     public static String format(String item, int quantity, double price) {
-        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
-        throw new RuntimeException("Not implemented");
-    }
+       // Format the price with a pound (£) symbol and two decimal places
+       String formattedPrice = String.format("£%.2f", price);
+       // Format the output string with item, quantity, and price
+       return String.format("Item: %s. Price: %s. Quantity: %d", item, formattedPrice,quantity);
+   }
+    
      
-
 }
