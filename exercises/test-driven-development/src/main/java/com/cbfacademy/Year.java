95f7dd49975e87bef5e893ddc67d1b03165680d5
Knya819 <knya819@gmail.com>
2024-08-23 11:59:12 +0100

    Modified Year.java

--- exercises/test-driven-development/src/main/java/com/cbfacademy/Year.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/Year.java
@@ -1,10 +1,15 @@
 package com.cbfacademy;
 
 public class Year { 
-    public static boolean isLeap(long i) {
-    if (i % 4 == 0 && i % 100 != 0) return true;
-    if (i % 100 == 0 && i % 5 != 0) return false;
-    else if (i % 400 == 0) return true;
+    private static Integer year;
+
+    public Year(Integer year){
+        Year.year = year;
+    }
+    public static boolean isLeapYear() {
+    if (year % 4 == 0 && year % 100 != 0) return true;
+    else if (year % 100 == 0 && year % 400 != 0) return false;
+    else if (year % 400 == 0) return true;
     else return false;
     
     }
